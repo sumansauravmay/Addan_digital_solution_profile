90196bcad0e5af40da8d8b2c2fc43ad6c1cbccb0
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 20:36:00 +0530

    loading

--- userprofile/src/App.js
+++ userprofile/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import AllRoutes from "./routes/AllRoutes";
+import Loading from "./components/Loading"
 
 function App() {
   return (
