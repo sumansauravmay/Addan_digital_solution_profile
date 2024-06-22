c9cdef0400c771f2aaf0ab99bd850c4b7d92d2e9
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 12:43:33 +0530

    frontend base

---
+++ userprofile/src/routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/" element={<Login/>}></Route>
+    </Routes>
+  )
+}
+
+export default AllRoutes
