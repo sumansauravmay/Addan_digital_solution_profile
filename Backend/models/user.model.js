781d73312bea0daab5cecc823062632f6c5a0c45
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 20:41:08 +0530

    picture add

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -5,10 +5,14 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   password: { type: String, required: true },
 
-  photo: { type: String, default: "NA" },
+  photo: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
   pastExperience: { type: String, default: "NA" },
   skillSets: { type: String, default: "NA" },
-  education: { type: String, default: "NA" }
+  education: { type: String, default: "NA" },
 });
 
 const UserModel = mongoose.model("users", userSchema);
