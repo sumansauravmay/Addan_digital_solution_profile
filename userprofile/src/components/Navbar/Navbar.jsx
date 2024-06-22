fc0929217ba605fbfc02765bb8d836c77f629710
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 13:21:29 +0530

    Navbar

--- userprofile/src/components/Navbar/Navbar.jsx
+++ userprofile/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  Flex,
+  Spacer,
+  Heading,
+  ButtonGroup,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { GoHomeFill } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <Flex gap="2" mt={5} w={"70%"} ml={"250px"}>
+      <Box p="2">
+        <Link to="/">
+        <GoHomeFill size="40px" />
+        <p>Home</p>
+        </Link>
+      </Box>
+      <Spacer />
+      <ButtonGroup gap="5">
+        <Link to="/profile">
+        <Button colorScheme="teal">Profile</Button>
+        </Link>
+        <Link to="/register">
+        <Button colorScheme="teal">Sign Up</Button>
+        </Link>
+        <Link to="/">
+        <Button colorScheme="teal">Log in</Button>
+        </Link>
+       
+      </ButtonGroup>
+    </Flex>
+  );
+};
 
-export default Navbar
+export default Navbar;
