b4107842c3ba94709c11e93d28d84893d6df65b4
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 12:44:26 +0530

    All Routes

--- userprofile/src/routes/AllRoutes.jsx
+++ userprofile/src/routes/AllRoutes.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Register from "../components/Register";
+import Profile from "../components/Profile";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login/>}></Route>
-      <Route path="/" element={<Login/>}></Route>
-      <Route path="/" element={<Login/>}></Route>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
