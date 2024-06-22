bb56ac368f6e6f699017bcb6a5a74e14e5a7f3d6
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-22 20:51:07 +0530

    profile

--- userprofile/src/components/Profile/Profile.jsx
+++ userprofile/src/components/Profile/Profile.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  Stack,
+  Button,
+  ButtonGroup,
+  Heading,
+  Image,
+  Text,
+  CardBody,
+  Card,
+  Divider,
+  CardFooter,
+  Flex,
+} from "@chakra-ui/react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <Card maxW="70%"
+    m="50px auto auto auto"
+    >
+      <CardBody>
+        <Image
+          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt="Green double couch with wooden legs"
+          borderRadius="lg"
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">Living room Sofa</Heading>
+          <Text>
+            This sofa is perfect for modern tropical spaces, baroque inspired
+            spaces, earthy toned spaces and for people who love a chic design
+            with a sprinkle of vintage design.
+          </Text>
+          <Text color="blue.600" fontSize="2xl">
+            $450
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Flex gap="5">
+          <Button variant="solid" colorScheme="blue">
+            Update
+          </Button>
+          <Button variant="solid" colorScheme="blue">
+            Delete
+          </Button>
+          <Button variant="solid" colorScheme="blue">
+            Logout
+          </Button>
+        </Flex>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default Profile;
